f134365b423c98520bb9cdd8dcd7ecf8e81bc5de
wkyou <wkyou@iflytek.com>
2022-08-02 10:42:29 +0800

    feat: effect新增 scheduler stop onStop功能

--- src/reactive/__test__/effect.spec.ts
+++ src/reactive/__test__/effect.spec.ts
@@ -1,4 +1,4 @@
-import { effect } from "../src/effect";
+import { effect, stop } from "../src/effect";
 import { reactive } from "../src/reactive";
 
 describe("effect", () => {
@@ -26,5 +26,64 @@ describe("effect", () => {
     const r = runner();
     expect(r).toBe("foo");
     expect(foo).toBe(12);
+  });
+
+  it("scheduler", () => {
+    let dummy;
+    let run: any;
+    const scheduler = jest.fn(() => {
+      run = runner;
+    });
+
+    // effect options有个scheduler属性
+    // 在构建effect时 执行一次fn收集依赖 scheduler不会执行
+    // 当依赖改变时, 执行scheduler 不会执行fn
+
+    const obj = reactive({ foo: 1 });
+    const runner = effect(
+      () => {
+        dummy = obj.foo;
+      },
+      { scheduler }
+    );
+
+    expect(scheduler).not.toHaveBeenCalled();
+    expect(dummy).toBe(1);
+
+    obj.foo++;
+    expect(scheduler).toHaveBeenCalledTimes(1);
+    expect(dummy).toBe(1);
+
+    run();
+    expect(dummy).toBe(2);
+  });
+
+  it("stop", () => {
+    let dummy;
+    const obj = reactive({ prop: 1 });
+    const runner = effect(() => {
+      dummy = obj.prop;
+    });
+
+    obj.prop = 2;
+    expect(dummy).toBe(2);
+    stop(runner);
+
+    obj.prop = 3;
+    expect(dummy).toBe(2);
+
+    // stopped effect should still be manually callable
+    runner();
+    expect(dummy).toBe(3);
+  });
+
+  it("events: onStop", () => {
+    const onStop = jest.fn();
+    const runner = effect(() => {}, {
+      onStop,
+    });
+
+    stop(runner);
+    expect(onStop).toHaveBeenCalled();
   });
 });
--- src/reactive/src/effect.ts
+++ src/reactive/src/effect.ts
@@ -1,30 +1,66 @@
+import { extend } from "../../shared";
+
 class ReactiveEffect {
   private _fn: any;
-  constructor(fn) {
+  deps: any[] = [];
+  active = true;
+  onStop?: () => void;
+  constructor(fn, public scheduler?: any) {
     this._fn = fn;
   }
   run() {
     activeEffect = this;
     return this._fn();
   }
+
+  // 从依赖dep内删除该effect
+  // 怎么知道effect存在哪个dep呢？
+  // 需要在收集依赖时 反向绑定一下dep
+  stop() {
+    // 频繁调用stop时 会多次清空
+    if (this.active) {
+      cleanupEffect(this);
+      if (this.onStop) {
+        this.onStop();
+      }
+      this.active = false;
+    }
+  }
+}
+
+function cleanupEffect(effect) {
+  effect.deps.forEach((dep) => {
+    dep.delete(effect);
+  });
 }
 
 // 正在执行的effect
 let activeEffect: any = null;
 
-export function effect(fn) {
+export function effect(fn, options: any = {}) {
   // effect 收集的fn需要执行一次
   // 创建一个EffectReactive类
-  const _effect = new ReactiveEffect(fn);
+  const _effect = new ReactiveEffect(fn, options?.scheduler);
+  // 合并options
+  // Object.assign(_effect, options);
+  extend(_effect, options);
 
+  const runner: any = _effect.run.bind(_effect);
+  runner.effect = _effect;
   _effect.run();
 
-  return _effect.run.bind(_effect);
+  return runner;
+}
+
+export function stop(runner) {
+  runner.effect.stop();
 }
 
 const targetMap = new Map();
 export function track(target, key) {
   // target -> key -> dep
+
+  if (!activeEffect) return;
   let depsMap = targetMap.get(target);
   if (!depsMap) {
     depsMap = new Map();
@@ -37,6 +73,11 @@ export function track(target, key) {
   }
 
   deps.add(activeEffect);
+  // 将当前effect 属于哪个dep 注入到effect内
+  // 方便操作effect实例时 知道哪些依赖
+
+  // 如果是单纯的reactive响应式时 activeEffect是undefined
+  activeEffect.deps.push(deps);
 }
 
 export function trriger(target, key) {
@@ -44,6 +85,11 @@ export function trriger(target, key) {
   const deps = depsMap.get(key);
 
   deps.forEach((effect) => {
-    effect.run();
+    // 在update的时候 要去检查effect是否有调度器 scheduler
+    if (effect.scheduler) {
+      effect.scheduler();
+    } else {
+      effect.run();
+    }
   });
 }

9e149d2dd594f367596a50324f7ba8cef1143d1f
wkyou <wkyou@iflytek.com>
2022-08-18 15:43:31 +0800

    feat: 子节点移动逻辑

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -244,9 +244,16 @@ export function createRenderer(options) {
       // 而是直接删除
       let patched = 0;
       const toBePatched = e2 - s2 + 1;
+      // 子节点是否移动 否: 不需要执行最长递增子序列函数 优化性能
+      let moved = false;
+      let maxNewIndexSoFar = 0;
 
       // 建立新的自己点映射表 遍历新子节点数组
       const keyToNewIndexMap = new Map();
+
+      const newIndexToOldIndexMap = new Array(toBePatched);
+      for (let i = 0; i < toBePatched; i++) newIndexToOldIndexMap[i] = 0;
+
       for (let i = s2; i <= e2; i++) {
         const nextChild = c2[i];
         keyToNewIndexMap.set(nextChild.key, i);
@@ -273,10 +280,45 @@ export function createRenderer(options) {
         }
 
         if (newIndex) {
+          if (newIndex > maxNewIndexSoFar) {
+            maxNewIndexSoFar = newIndex;
+          } else {
+            // 有节点需要移动
+            moved = true;
+          }
+
+          // 这里的i + 1是避免和初始化的0重复
+          newIndexToOldIndexMap[newIndex - s2] = i + 1;
           patch(prevChild, c2[newIndex], container, parentComponent, null);
           patched++;
         } else {
           hostRemove(prevChild.el);
+        }
+      }
+
+      const increasingNewIndexSequence = moved
+        ? getSequence(newIndexToOldIndexMap)
+        : [];
+      console.log("最长递增子序列:", increasingNewIndexSequence);
+
+      // 移动的方法是hostInsert 需要知道插入位置
+      // 正序的话 无法保证前面的节点是稳定的
+      // 所以需要倒序循环
+      let j = increasingNewIndexSequence.length - 1;
+      for (let i = toBePatched - 1; i >= 0; i--) {
+        const nextIndex = i + s2;
+        const nextChild = c2[nextIndex];
+        const anchor = nextIndex + 1 < c2.length ? c2[nextIndex + 1].el : null;
+        if (newIndexToOldIndexMap[i] === 0) {
+          // 如果等于0 需要创建节点
+          patch(null, nextChild, container, parentComponent, anchor);
+        } else if (moved) {
+          if (j < 0 || i !== increasingNewIndexSequence[j]) {
+            console.log("需要移动");
+            hostInsert(nextChild.el, container, anchor);
+          } else {
+            j--;
+          }
         }
       }
     }
@@ -335,4 +377,45 @@ export function createRenderer(options) {
   return {
     createApp: createAppAPI(render),
   };
+}
+
+function getSequence(arr: number[]): number[] {
+  const p = arr.slice();
+  const result = [0];
+  let i, j, u, v, c;
+  const len = arr.length;
+  for (i = 0; i < len; i++) {
+    const arrI = arr[i];
+    if (arrI !== 0) {
+      j = result[result.length - 1];
+      if (arr[j] < arrI) {
+        p[i] = j;
+        result.push(i);
+        continue;
+      }
+      u = 0;
+      v = result.length - 1;
+      while (u < v) {
+        c = (u + v) >> 1;
+        if (arr[result[c]] < arrI) {
+          u = c + 1;
+        } else {
+          v = c;
+        }
+      }
+      if (arrI < arr[result[u]]) {
+        if (u > 0) {
+          p[i] = result[u - 1];
+        }
+        result[u] = i;
+      }
+    }
+  }
+  u = result.length;
+  v = result[u - 1];
+  while (u-- > 0) {
+    result[u] = v;
+    v = p[v];
+  }
+  return result;
 }
